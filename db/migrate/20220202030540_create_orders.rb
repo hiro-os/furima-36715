406c504baf319ef2063711f0878c181c8080ea0f
hiro-os <73930571+hiro-os@users.noreply.github.com>
2022-02-03 21:53:49 +0900

    新規登録、ログイン、ログアウトの実装

--- db/migrate/20220202030540_create_orders.rb
+++ db/migrate/20220202030540_create_orders.rb
@@ -1,10 +1,10 @@
-#class CreateOrders < ActiveRecord::Migration[6.0]
-#  def change
-#    create_table :orders do |t|
-#      t.integer :item_id
-#      t.integer :user_id
+class CreateOrders < ActiveRecord::Migration[6.0]
+  def change
+    create_table :orders do |t|
+      t.integer :item_id
+      t.integer :user_id
 
-#      t.timestamps
-#    end
-#  end
-#end
+      t.timestamps
+    end
+  end
+end
--- db/migrate/20220202031751_create_addresses.rb
+++ db/migrate/20220202031751_create_addresses.rb
@@ -1,15 +1,15 @@
-#class CreateAddresses < ActiveRecord::Migration[6.0]
-#  def change
-#    create_table :addresses do |t|
-#      t.string :postcode
-#      t.integer :area_id
-#      t.string :city
-#      t.string :street
-#      t.string :building
-#      t.string :phone
-#      t.integer :order_id
+class CreateAddresses < ActiveRecord::Migration[6.0]
+  def change
+    create_table :addresses do |t|
+      t.string :postcode
+      t.integer :area_id
+      t.string :city
+      t.string :street
+      t.string :building
+      t.string :phone
+      t.integer :order_id
 
-#      t.timestamps
-#    end
-#  end
-#end
+      t.timestamps
+    end
+  end
+end

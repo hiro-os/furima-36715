3858a2b2cede4d8d951491cc789ff2c2738c2bec
hiro-os <73930571+hiro-os@users.noreply.github.com>
2022-02-08 21:44:13 +0900

    使わないイグレーションファイルにコメントアウト

--- db/migrate/20220202031751_create_addresses.rb
+++ db/migrate/20220202031751_create_addresses.rb
@@ -1,15 +1,15 @@
-class CreateAddresses < ActiveRecord::Migration[6.0]
-  def change
-    create_table :addresses do |t|
-      t.string :postcode
-      t.integer :area_id
-      t.string :city
-      t.string :street
-      t.string :building
-      t.string :phone
-      t.references :order, null: false, foreign_key: true
-      
-      t.timestamps
-    end
-  end
-end
+#class CreateAddresses < ActiveRecord::Migration[6.0]
+#  def change
+#    create_table :addresses do |t|
+#      t.string :postcode
+#      t.integer :area_id
+#      t.string :city
+#      t.string :street
+#      t.string :building
+#      t.string :phone
+#      t.references :order, null: false, foreign_key: true
+
+#      t.timestamps
+#    end
+#  end
+#end

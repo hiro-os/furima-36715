950bd53ad458a1dfb7c79cd34cdf5b37699a4ff8
hiro-os <73930571+hiro-os@users.noreply.github.com>
2022-02-09 20:52:01 +0900

    destroyアクションを定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new,:edit]
   before_action :set_item,only: [:edit,:show,:update]
-  before_action :move_to_index, except: [:index, :show, :new, :create]
+  before_action :move_to_index, except: [:index, :show, :new, :create, :destroy]
   def index
     @items = Item.order(created_at: :DESC)
   end
@@ -31,6 +31,12 @@ class ItemsController < ApplicationController
     else
       render :edit
     end
+  end
+
+  def destroy
+    item = Item.find(params[:id])
+    item.destroy
+    redirect_to action: :index
   end
 
   private
